854af2f00377d232a943fce8039468f231288aee
faisalalsudani <f4desiign@gmail.com>
2017-09-14 12:02:23 +0200

    Add API

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root "pages#home"
 
+    resources :photos
+
     resources :artists do
       resources :songs
     end
-    resources :photos
+    
+    namespace :api do
+      resources :artists do
+        resources :songs
+      end
+    end
 end
